ca75b7a6d619884a63b103a7d7b4446553d988ea
Andy Oakley <andy@andyoakley.com>
2018-09-22 20:46:51 -0700

    Update for mkdocs v1

--- mkdocs_blog/__init__.py
+++ mkdocs_blog/__init__.py
@@ -9,39 +9,41 @@ class Blog(BasePlugin):
     def parse_url(self, url):
         try:
             pieces = url.split('/')
-            year = int(pieces[1])
-            month = int(pieces[2])
+            year = int(pieces[0])
+            month = int(pieces[1])
             return (year, month)
         except ValueError:
             return None
+        except IndexError:
+            return None
 
-    def on_nav(self, site_navigation, config):
+    def on_nav(self, nav, config, files):
         # ordered by time
         ordered = []
         # nested by year and month
         chronological = {}
 
-        for page in site_navigation.walk_pages():
-            parsed = self.parse_url(page.abs_url)
+        for f in files:
+            parsed = self.parse_url(f.url)
             if parsed:
                 year, month = parsed
 
                 yeartime = datetime.datetime(year, 1, 1)
                 monthtime = datetime.datetime(year, month, 1)
-                mtime = os.path.getmtime(page.abs_input_path)
+                mtime = os.path.getmtime(f.abs_src_path)
 
-                ordered.append((page, year, month, mtime))
+                ordered.append((f.page, year, month, mtime))
 
                 if not yeartime in chronological:
                     chronological[yeartime] = {}
                 if not monthtime in chronological[yeartime]:
                     chronological[yeartime][monthtime] = {}
-                chronological[yeartime][monthtime][mtime] = page
+                chronological[yeartime][monthtime][mtime] = f.page
 
         ordered.sort(key=lambda tup: (tup[1], tup[2], tup[3]))
 
         config['ordered'] = ordered
         config['chronological'] = chronological
 
-        return site_navigation
+        return nav
 
